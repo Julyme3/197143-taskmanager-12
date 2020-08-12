9e5a6ff75239d9394e0d88c654a955e284cf312b
Julyme3 <jg0311@bk.ru>
2020-08-12 17:56:07 +0300

    Сделала компоненты классами

--- src/view/filter.js
+++ src/view/filter.js
@@ -1,22 +1,48 @@
-const createItemFilterTemplate = (filter, isChecked) => {
-  const {title, count} = filter;
-  return `<input
-      type="radio"
-      id="filter__${title}"
-      class="filter__input visually-hidden"
-      name="filter"
-      ${count ? `` : `disabled`}
-      ${isChecked ? `checked` : ``}
-    />
-    <label for="filter__${title}" class="filter__label">${title}
-      <span class="filter__all-count">${count}</span>
-    </label>
-  `;
-};
+import {createElement} from "../utils";
 
-const createFilterTemplate = (filters) =>
-  `<section class="main__filter filter container">
-    ${filters.map((filter, idx) => createItemFilterTemplate(filter, idx === 0)).join(``)}
-  </section>`
-;
-export {createFilterTemplate};
+export default class Filter {
+
+  constructor(filters) {
+    this._filters = filters;
+    this._element = null;
+  }
+
+  createItemTemplate(filter, isChecked) {
+    const {title, count} = filter;
+    return `<input
+        type="radio"
+        id="filter__${title}"
+        class="filter__input visually-hidden"
+        name="filter"
+        ${count ? `` : `disabled`}
+        ${isChecked ? `checked` : ``}
+      />
+      <label for="filter__${title}" class="filter__label">${title}
+        <span class="filter__all-count">${count}</span>
+      </label>`
+    ;
+  }
+
+  createTemplate(filters) {
+    return `<section class="main__filter filter container">
+        ${filters.map((filter, idx) => this.createItemTemplate(filter, idx === 0)).join(``)}
+      </section>`
+    ;
+  }
+
+  getTemplate() {
+    return this.createTemplate(this._filters);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+}
--- src/view/sort.js
+++ src/view/sort.js
@@ -1,8 +1,31 @@
-const createSortingTemplate = () =>
-  `<div class="board__filter-list">
-    <a href="#" class="board__filter">SORT BY DEFAULT</a>
-    <a href="#" class="board__filter">SORT BY DATE up</a>
-    <a href="#" class="board__filter">SORT BY DATE down</a>
-  </div>`
-;
-export {createSortingTemplate};
+import {createElement} from "../utils";
+
+export default class Sort {
+
+  constructor() {
+    this._element = null;
+  }
+
+  createTemplate() {
+    return `<div class="board__filter-list">
+      <a href="#" class="board__filter">SORT BY DEFAULT</a>
+      <a href="#" class="board__filter">SORT BY DATE up</a>
+      <a href="#" class="board__filter">SORT BY DATE down</a>
+    </div>`;
+  }
+
+  getTemplate() {
+    return this.createTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
